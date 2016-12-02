c9969ad7f5fb21abe1cacb77a40a84c5bd29b810
Bryan Yee <yee.bryan.c@gmail.com>
2016-12-02 00:53:53 -0800

    Append cookie strings to the DOM

--- client/index.js
+++ client/index.js
@@ -1,2 +1,16 @@
+// Set a cookie from the client
 document.cookie = `COOKIE_FROM_CLIENT=cookieFromClient;expires=${(new Date(Date.now() + 60000).toUTCString())}`;
-console.log(document.cookie);
+
+const cookies = document.cookie
+                .split(';')
+                .map(cookie => cookie.trim());
+
+// Append a list of cookies to the page
+const list = document.getElementById('cookie-list');
+
+cookies.forEach((cookie) => {
+  const item = document.createElement('li');
+  const text = document.createTextNode(cookie);
+  item.appendChild(text);
+  list.appendChild(item);
+});
